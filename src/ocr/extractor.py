2177e3747f1ae7ff94b427102218ab06b0c7e0f2
Yves Ngah <rybsngah@yahoo.fr>
2026-05-19 03:44:50 +0200

    testing extractor

--- src/ocr/extractor.py
+++ src/ocr/extractor.py
@@ -1,38 +1,33 @@
-#src/ocr/extractor.py
-#pip install easyocr
+# src/ocr/extractor.py
 import easyocr
 import numpy as np
 from src.config.logging_config import get_logger
 
-#Calling the logger
-logger= get_logger("extraction")
+logger = get_logger("extraction")
 
 class OCREngine:
     def __init__(self, languages=["en", "fr"], gpu=True):
-
         logger.info(f"Initialize EasyOCR for languages : {languages}")
-        # Weights are initialised one time
         self.reader = easyocr.Reader(languages, gpu=gpu)
 
-        def extract_text(self, image_path: str):
-            """
-            tract text from image and return chain of characters
-            """
-            try:
-                logger.info(f"arting ocr extraction for : {image_path}")
-                results = self.reader.readtext(image_path)
-            
-                # EasyOCR return tuples : (bbox, text, confidence)
-                extracted_lines = [res[1] for res in results]
-                full_text = " ".join(extracted_lines)
-            
-                logger.info(f"number of lines detected : {len(extracted_lines)}")
-                return full_text
-            
-            except Exception as e:
-                logger.error(f"Error in OCR process : {e}")
+    def extract_text(self, image_path: str):
+        """
+        extract text from image and return chain of characters
+        """
+        try:
+            logger.info(f"starting ocr extraction for : {image_path}")
+            results = self.reader.readtext(image_path)
+
+            extracted_lines = [res[1] for res in results]
+            full_text = " ".join(extracted_lines)
+
+            logger.info(f"number of lines detected : {len(extracted_lines)}")
+            return full_text
+
+        except Exception as e:
+            logger.error(f"Error in OCR process : {e}")
             raise e
-#Testing our extractor
+
 if __name__ == "__main__":
     engine = OCREngine()
     text = engine.extract_text("data/samples/2078153623.tif")
---
+++ src/ocr/preprocess.py
@@ -0,0 +1,19 @@
+import re
+
+def clean_text(text: str) -> str:
+    """
+    cleaning raw text from OCR.
+    """
+    if not text:
+        return ""
+
+    # 1. Delete special characters
+    text = re.sub(r'[^\w\s.,€$]', '', text)
+    
+    # 2. Replace lines break by spaces
+    text = text.replace('\n', ' ').replace('\t', ' ')
+    
+    # 3. Delete multiples spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+    
+    return text
