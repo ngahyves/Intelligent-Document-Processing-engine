e688c0158587af27a1290ebb6fe0ca479ee38bbf
Yves Ngah <rybsngah@yahoo.fr>
2026-05-19 05:23:54 +0200

    chunker and embeddings

---
+++ rag/chunker.py
@@ -0,0 +1,24 @@
+# rag/chunker.py
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from src.config.logging_config import get_logger
+
+logger=get_logger('chunking')
+
+class DocumentChunker:
+    def __init__(self, chunk_size=500, chunk_overlap=50):
+        """
+        chunk_size: number of character by chunks.
+        chunk_overlap: to avoid chunking and lose the sens of the sentence
+        """
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+            separators=["\n\n", "\n", ".", " ", ""]
+        )
+
+    def split_text(self, text: str):
+        logger.info("chunking's start.")
+        chunks = self.splitter.split_text(text)
+        logger.info(f"Text splitted in {len(chunks)} chunks.")
+        return chunks
---
+++ rag/embedder.py
@@ -0,0 +1,25 @@
+# rag/embeddings_utils.py
+
+from sentence_transformers import SentenceTransformer
+from src.config.logging_config import get_logger
+
+logger=get_logger('Embedding')
+
+class TextEmbedder:
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        logger.info(f"loading embedding model : {model_name}")
+        self.model = SentenceTransformer(model_name)
+
+    def embed_chunks(self, chunks: list):
+        """
+        Transforme text's list in vector.
+        """
+        logger.info(f"Generate embeddings for {len(chunks)} chunks.")
+        embeddings = self.model.encode(chunks, show_progress_bar=False)
+        return embeddings
+
+    def embed_query(self, query: str):
+        """
+        Transform a user's question in vector.
+        """
+        return self.model.encode([query])[0]
