155d6e2c8a93f984d5f918eef33439d177c8934f
Yves Ngah <rybsngah@yahoo.fr>
2026-05-20 12:11:00 +0200

    API test fixing since model is not on github

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,21 @@
 from fastapi.testclient import TestClient
-from api.main import app # importing the app
+import unittest.mock as mock
+import pytest
 
-# Create a false client
+#Simulate the call of the model
+with mock.patch("onnxruntime.InferenceSession"):
+    with mock.patch("src.vision.classifier.DocumentClassifier"):
+        from api.main import app
+
 client = TestClient(app)
 
 def test_health():
-    """verifying the health check"""
+    """Vérify if API is healthy(running)"""
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json()["status"] == "healthy"
 
 def test_ask_no_question():
-    """API send an error if no question is asked"""
-    # Empty request
+    """Vérify if the API rejects empty requests"""
     response = client.post("/ask", json={})
-    # 422 means incorrect data
     assert response.status_code == 422
